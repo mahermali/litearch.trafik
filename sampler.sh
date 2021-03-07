aee5c59f0d7a9dd31291f03baa068e30b0603ffd
Maher ali <maher.techmasters@gmail.com>
2021-03-07 09:47:48 -0500

    make sampler script more resilient

--- sampler.sh
+++ sampler.sh
@@ -5,7 +5,7 @@ function sample(){
     echo "Sample ... "  
     echo "$host"
     docker inspect -f '{{.NetworkSettings.IPAddress }}' $(docker ps -q) | awk -vhostname="$(hostname)" 'NF {printf "$:%s:%s\n", hostname, $1}'
-    docker ps --format "{{.Ports}}" | awk -vORS=' or '   -F [:-] 'IF $2 {print $2}' | sed '$s/...$//' | xargs -0 -I {} bash -c "timeout 3 sudo tcpdump tcp -nn -c 100 -i any port '({} and not 50777)'" | awk '{gsub(/:/,"");print "#"$3"$"$5} ' | nc -q 1 "${host}" 50777
+    docker ps --format "{{.Ports}}" | awk -vORS=' or '   -F [:-] 'IF $2 {print $2}' | sed '$s/...$//' | xargs -0 -I {} bash -c "timeout 3 sudo tcpdump tcp -nn -c 100 -i any port '({} and not 50777)'" | awk '{gsub(/:/,"");print "#"$3"$"$5} '
 }
 
-(while true; do sample ; sleep 3; done)
+(while true; do sample | nc -q 1 "${host}" 50777; sleep 3; done)
