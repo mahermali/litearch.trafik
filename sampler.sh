e54966cb89337470c2f5d23a5f29f2439d0a4b02
Maher ali <maher.techmasters@gmail.com>
2021-03-07 00:34:16 -0500

    make sampler script more resilient

--- sampler.sh
+++ sampler.sh
@@ -4,7 +4,7 @@ function sample(){
     host=$(docker inspect -f '{{.Config.Env}}' $(docker ps --filter publish=50777 -q) |  grep -oP '(?<=NOMAD_HOST_IP_tcp=).*?(?= )')  
     echo $host
     docker inspect -f '{{.NetworkSettings.IPAddress }}' $(docker ps -q) | awk -vhostname="$(hostname)" 'NF {printf "$:%s:%s\n", hostname, $1}'
-    docker ps --format "{{.Ports}}" | awk -vORS=' or '   -F [:-] 'IF $2 {print $2}' | sed '$s/...$//' | xargs -0 -I {} bash -c "timeout 5 sudo tcpdump tcp -nn -c 100 -i any port '({} and not 50777)'" | awk '{gsub(/:/,"");print "#"$3"$"$5} '
+    docker ps --format "{{.Ports}}" | awk -vORS=' or '   -F [:-] 'IF $2 {print $2}' | sed '$s/...$//' | xargs -0 -I {} bash -c "timeout 5 sudo tcpdump tcp -nn -c 100 -i any port '({} and not 50777)'" | awk '{gsub(/:/,"");print "#"$3"$"$5} ' |  nc "${host}" 50777
 }
 
-(while true; do sample ; sleep 3; done) | (while true; do nc "${host}" 50777; done)
+(while true; do sample ; sleep 3; done)
