8f413050ceeede9040763d82a72ae42dc777de5f
Maher ali <maher.techmasters@gmail.com>
2021-03-07 20:34:26 -0500

    fix smapler.sh

--- sampler.sh
+++ sampler.sh
@@ -1,11 +1,12 @@
+#!/bin/bash
 declare host="0.0.0.0"
 
-function sample(){  
-    host=$(docker inspect -f '{{.Config.Env}}' $(docker ps --filter publish=50777 -q) |  grep -oP '(?<=NOMAD_HOST_IP_tcp=).*?(?= )')
-    echo "Sample ... "  
-    echo "$host"
-    docker inspect -f '{{.NetworkSettings.IPAddress }}' $(docker ps -q) | awk -vhostname="$(hostname)" 'NF {printf "$:%s:%s\n", hostname, $1}'
-    docker ps --format "{{.Ports}}" | awk -vORS=' or '   -F [:-] 'IF $2 {print $2}' | sed '$s/...$//' | xargs -0 -I {} bash -c "timeout 3 sudo tcpdump tcp -nn -c 100 -i any port '({} and not 50777)'" | awk '{gsub(/:/,"");print "#"$3"$"$5} '
-}
-
-(while true; do sample; sleep 3; done;) | (while true; do nc -q 1 -N "${host}" 50777; done;)
+while true; do
+      printf "Start .."
+      host=$(docker inspect -f '{{.Config.Env}}' $(docker ps --filter publish=50777 -q) |  grep -oP '(?<=NOMAD_HOST_IP_tcp=).*?(?= )')
+      printf "Host: $s" "$host"
+      docker inspect -f '{{.NetworkSettings.IPAddress }}' $(docker ps -q) | awk -vhostname="$(hostname)" 'NF {printf "$:%s:%s\n", hostname, $1}'
+      docker ps --format "{{.Ports}}" | awk -vORS=' or ' -F [:-] 'IF $2 {print $2}' | sed '$s/...$//' | xargs -0 -I {} bash -c "timeout 3 tcpdump tcp -nn -c 100 -i any port '({} and not 50777)'" | awk '{gsub(/:/,"");print "#"$3"$"$5} ' > /dev/tcp/"${host}"/50777
+      printf "Iteration done"
+      sleep 3
+done
