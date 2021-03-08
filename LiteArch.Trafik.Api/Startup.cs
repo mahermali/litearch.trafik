0810ee5178f23a3e7e5747c4229625a46320c282
Maher ali <maher.techmasters@gmail.com>
2021-03-07 21:43:25 -0500

    add cors to api project

--- LiteArch.Trafik.Api/Startup.cs
+++ LiteArch.Trafik.Api/Startup.cs
@@ -27,6 +27,7 @@ namespace LiteArch.Trafik.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddControllers();
             services.AddHealthChecks();
             services.AddTransient<ITopologyService, TopologyService>();
@@ -36,7 +37,8 @@ namespace LiteArch.Trafik.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-
+            app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+            
             app.UseRouting();
 
             app.UseAuthorization();
