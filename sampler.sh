048549908934c1cfa827dc2351751d9235528baf
Maher ali <maher.techmasters@gmail.com>
2021-03-07 19:41:26 -0500

    fix smapler.sh

--- sampler.sh
+++ sampler.sh
@@ -8,4 +8,4 @@ function sample(){
     docker ps --format "{{.Ports}}" | awk -vORS=' or '   -F [:-] 'IF $2 {print $2}' | sed '$s/...$//' | xargs -0 -I {} bash -c "timeout 3 sudo tcpdump tcp -nn -c 100 -i any port '({} and not 50777)'" | awk '{gsub(/:/,"");print "#"$3"$"$5} '
 }
 
-(while true; do sample; sleep 3; done;) | (while true; nc -q 1 -N "${host}" 50777; done;)
+(while true; do sample; sleep 3; done;) | (while true; do nc -q 1 -N "${host}" 50777; done;)
