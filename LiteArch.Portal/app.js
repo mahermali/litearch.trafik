2d9b39d53c118043983daea508b576daa198bd83
Maher ali <maher.techmasters@gmail.com>
2021-03-08 00:29:44 -0500

    add portal prefix

--- LiteArch.Portal/app.js
+++ LiteArch.Portal/app.js
@@ -11,6 +11,17 @@ const app = express();
 
 app.use(express.static('public'));
 
+var fs = require('fs')
+fs.readFile('index.html', 'utf8', function (err,data) {
+    if (err) {
+        return console.log(err);
+    }
+    var result = data.replace(/<%=TRAFIK_PORTAL_PREFIX%>/g, process.env.TRAFIK_PORTAL_PREFIX);
+
+    fs.writeFile('index.html', result, 'utf8', function (err) {
+        if (err) return console.log(err);
+    });
+});
 
 app.get('/', (req, res) => {
     res.sendFile('./index.html', { root: __dirname });
