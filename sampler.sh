18188a99135d2e38d257f7f8748bb6bdce9c7ae9
Maher ali <maher.techmasters@gmail.com>
2021-03-07 21:21:46 -0500

    add som debugging

--- sampler.sh
+++ sampler.sh
@@ -5,8 +5,7 @@ while true; do
       printf "Start ..\n"
       host=$(docker inspect -f '{{.Config.Env}}' $(docker ps --filter publish=50777 -q) |  grep -oP '(?<=NOMAD_HOST_IP_tcp=).*?(?= )')
       printf "Host: $s" "$host"
-      docker inspect -f '{{.NetworkSettings.IPAddress }}' $(docker ps -q) | awk -vhostname="$(hostname)" 'NF {printf "$:%s:%s\n", hostname, $1}' > /dev/tcp/"${host}"/50777
-      docker ps --format "{{.Ports}}" | awk -vORS=' or ' -F [:-] 'IF $2 {print $2}' | sed '$s/...$//' | xargs -0 -I {} bash -c "timeout 3 tcpdump tcp -nn -c 100 -i any port '({} and not 50777)'" | awk '{gsub(/:/,"");print "#"$3"$"$5} ' > /dev/tcp/"${host}"/50777
+      { docker inspect -f '{{.NetworkSettings.IPAddress }}' $(docker ps -q) | awk -vhostname="$(hostname)" 'NF {printf "$:%s:%s\n", hostname, $1}' ; docker ps --format "{{.Ports}}" | awk -vORS=' or ' -F [:-] 'IF $2 {print $2}' | sed '$s/...$//' | xargs -0 -I {} bash -c "timeout 3 tcpdump tcp -nn -c 100 -i any port '({} and not 50777)'" | awk '{gsub(/:/,"");print "#"$3"$"$5} ' ; } > /dev/tcp/"${host}"/50777
       printf "Iteration done\n"
       sleep 3
 done
